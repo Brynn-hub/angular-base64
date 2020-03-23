6805e1c8d108973ba91715bf5cfd2e96d8aa25ac
br126 <wanglubr@126.com>
2020-03-23 22:08:45 +0800

    declare variable type

--- src/app/area/area.component.ts
+++ src/app/area/area.component.ts
@@ -17,22 +17,22 @@ export class AreaComponent implements OnInit {
   ngOnInit(): void {
   }
 
-  async handleInput(e) {
+  async handleInput(e: any) {
     this.setImgSrc.emit(await this.FileRenderService.getBase64(e.target.files[0]));
   }
   handleButton() {
     this.handleButtonProps.emit();
   }
 
-  async handleDrop(e) {
+  async handleDrop(e: any) {
     e.preventDefault();
     this.setImgSrc.emit(await this.FileRenderService.getBase64(e.dataTransfer.files[0]));
   }
-  handleDragover(e) {
+  handleDragover(e: any) {
     e.preventDefault();
   }
 
-  async handlePaste(e) {
+  async handlePaste(e: any) {
     this.setImgSrc.emit(await this.FileRenderService.getBase64(e.clipboardData.files[0]));
   }
 }
